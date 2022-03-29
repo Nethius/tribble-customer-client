ffc94690f13729a525fcda9ede16d02d61449d45
aleksey.ermolaev <zich.alexey@gmail.com>
2022-03-29 22:11:39 +0500

    configure project

---
+++ Source/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Tribble-App
+//
+//  Created by aleksejermolaev on 29.03.2022.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		return true
+	}
+}
